d341e4e0a7eb5d15f30793e38cafaefe89bc0c72
arroy <arroy@DESKTOP-FDRGAC6>
2022-03-16 19:17:44 -0600

    Add project files.

---
+++ NatalieArroyo_Parcial1_Progra6-2022-1_APIRest/Models/Activo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NatalieArroyo_Parcial1_Progra6_2022_1_APIRest.Models
+{
+    public partial class Activo
+    {
+        public int Idactivo { get; set; }
+        public string NombreActivo { get; set; } = null!;
+        public string? NumeroSerie { get; set; }
+        public decimal CostoActivo { get; set; }
+        public string? Descripcion { get; set; }
+        public string Ubicacion { get; set; } = null!;
+        public string Responsable { get; set; } = null!;
+        public DateTime FechaRegistro { get; set; }
+        public bool? EstadoActivo { get; set; }
+    }
+}
---
+++ NatalieArroyo_Parcial1_Progra6-2022-1_APIRest/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace NatalieArroyo_Parcial1_Progra6_2022_1_APIRest
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
